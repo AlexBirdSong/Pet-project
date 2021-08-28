f596ec4cf332577e31c76ba7ba07308aac22c3b7
AlexBirdSong <alexwhitechoco@gmail.com>
2021-08-28 17:59:14 +0300

    added visualization choise

--- rock paper scissors and a well is also needed/app.js
+++ rock paper scissors and a well is also needed/app.js
@@ -14,23 +14,80 @@ const game = () => {
     };
 
     const playMatch = () => {
-        const options = document.querySelectorAll('.options button');
-        const playerHand = document.querySelectorAll('.player-hand');
-        const computerHand = document.querySelectorAll('.computer-hand');
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.getElementById("player-hand");
+        const computerHand = document.getElementById("computer-hand");
 
-        const computerOptions = ['rock', 'paper', 'scissors', 'wall'];
+        const computerOptions = ['Камень', 'Бумага', 'Ножницы', 'Колодец'];
 
         options.forEach(option => {
             option.addEventListener('click', function(){
                 const computerNumber = Math.floor( Math.random() * 4);
-                const computerChoise = computerOptions[computerNumber];
-                console.log(computerChoise);
-            });
-        });
+                const computerChoice = computerOptions[computerNumber];
 
-   
-     
-    }
+                playerHand.src = `sprites/${this.textContent}.png`;
+                computerHand.src = `sprites/${computerChoice}.png`;
+      
+            });
+        });             
+    };
+
+
+
+      
+
+    const compareHands = (playerChoice, computerChoice) => {
+
+        const winner = document.querySelector('.winner');
+
+        if(playerChoice === computerChoice){
+            winner.textContent = 'Ничья!';
+            return;
+        }  
+        if(playerChoice === 'Камень'){
+            if(computerChoice === 'Ножницы'){
+                winner.textContent = 'Победил игрок!';
+                return;
+            }
+            else{
+                winner.textContent = 'Победил компьютер!';
+                return;
+            }
+        }
+
+        if(playerChoice === 'Бумага'){
+            if(computerChoice === 'Ножницы'){
+                winner.textContent = 'Победил компьютер!';
+                return;
+            }
+            else{
+                winner.textContent = 'Победил игрок!';
+                return;
+            }
+        }
+
+        if(playerChoice === 'Колодец'){
+            if(computerChoice === 'Бумага'){
+                winner.textContent = 'Победил компьютер!';
+                return;
+            }
+            else{
+                winner.textContent = 'Победил игрок!';
+                return;
+            }
+        }
+
+        if(playerChoice === 'Ножницы'){
+            if(computerChoice === 'Бумага'){
+                winner.textContent = 'Победил игрок!';
+                return;
+            }
+            else{
+                winner.textContent = 'Победил компьютер!';
+                return;
+            }
+        }
+    }  
 
     startGame();
     playMatch();
