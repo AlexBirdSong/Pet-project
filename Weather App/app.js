0318ec33ee615e3a3ed14d443e8b021a996fa1a0
AlexBirdSong <alexwhitechoco@gmail.com>
2021-09-16 22:35:59 +0300

    getting feedback from api

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
            long = position.coords.longitude;
            lat = position.coords.latitude;
            
+           const proxy = "https://cors-anywhere.herokuapp.com/";
            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=84cdb70b7c1fe5c8ffab933c10b40a5c`;
     
 
@@ -15,8 +19,15 @@ window.addEventListener('load', ()=>{
                 return response.json();
             })
             .then(data =>{
-                console.log(data);
-            })
+                const {description} = data.weather[0];
+                const {name} = data;
+                const {temp} = data.main;
+
+                let degreeConvertion = Math.round((temp - 273.15),2);
+                locationTimezone.textContent = name;
+                temperatureDescription.textContent = description;
+                temperatureDegree.textContent = degreeConvertion;
+            });
 
         });
 
